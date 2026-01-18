56c7f3dad1f497b329f84cb7b5df638c6f644590
Bálint Gaganetz <balint.gaganetz@gmail.com>
2026-01-18 19:47:02 +0100

    identity tables added

--- MovieClubX.Data/Migrations/20260118184559_identity_added.Designer.cs
+++ MovieClubX.Data/Migrations/20260118184559_identity_added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieClubX.Data;
 
@@ -11,9 +12,11 @@ using MovieClubX.Data;
 namespace MovieClubX.Data.Migrations
 {
     [DbContext(typeof(MovieClubContext))]
-    partial class MovieClubContextModelSnapshot : ModelSnapshot
+    [Migration("20260118184559_identity_added")]
+    partial class identity_added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
